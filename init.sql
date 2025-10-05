5794c3c929cdf47c63de4067c757b33a420a4582
SanguraKoded <sanguraj12@gmail.com>
2025-10-05 14:22:41 +0300

    Integrated Liquibase database migration

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE assignment_service;
-CREATE DATABASE auth_service;
-CREATE DATABASE errand_service;
-CREATE DATABASE tracking_service;
+CREATE DATABASE assignment_service_db;
+CREATE DATABASE auth_service_db;
+CREATE DATABASE errand_service_db;
+CREATE DATABASE tracking_service_db;
 CREATE USER keycloak WITH PASSWORD 'keycloakpass';
 CREATE DATABASE keycloak;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
